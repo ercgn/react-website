3a04ddaee60acf1c13792e171316d3f297ffc851
Eric <eric@duolingo.com>
2020-09-09 23:32:03 -0700

    Update website

---
+++ README.md
@@ -0,0 +1,15 @@
+# Overview
+
+Personal website developed on React.
+
+To start, run 
+
+    $ npm install
+    $ npm run build
+    $ npm run server-dev
+
+After building, open another terminal window and run 
+
+    $ npm run react-dev
+
+The website should then be live at `localhost:8080`.
--- react-client/src/components/welcomePanel.jsx
+++ react-client/src/components/welcomePanel.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
 import styles from '../assets/styles/main.less';
-import resumePdf from '../assets/resume2019.pdf';
 
 function createWelcomeHeader() {
   return (
     <header>
-      <p id={styles.portfolio}>Eric Gan</p>
+      <p className={styles.portfolio}>Eric Gan</p>
       <p>I am passionate about developing software for the social good.
         Whether it is developing mobile apps for humanitarian efforts or
         designing a more intuitive UX/UI for a non-profit company, I find
             technology a gateway to unifying the international world.</p>
 
-      <p>You can find me at Microsoft working on PowerApps. Or you can
-            find me developing some apps for non-profits during my spare time!</p>
-
-      <ul className={styles.listButtons}>
-        {/* <li><Button>Projects</Button></li> */}
-        {/* <li><a target="_blank" href={resumePdf}><Button>Resume</Button></a></li> */}
+      <p>You can find me as a</p>
+      <ul>
+        <li><strong>Full-stack developer</strong> at <a href="https://www.duolingo.com">Duolingo</a> working on <a href="https://events.duolingo.com">Duolingo Events</a>.</li>
+        <li><strong>Regional director</strong> of <a href="https://www.basiltech.org">BasilTech</a>, leading a group of volunteers in Seattle to consult with non-profit humanitarian organizations.</li>
+        <li><strong>Licensed real estate agent</strong> for <a href="https://ericgan.skylineproperties.com">Skyline Properties</a>, specializing in investment properties.</li>
       </ul>
+      Send me an <a href="mailto:me@ericgan.com">email</a> if you want to connect!
     </header>
   );
 }
@@ -39,7 +37,6 @@ function createWelcomeFooter() {
         </a>
       </li>
     </ul>
-    <p>Theme and design made using <a href="https://reactjs.org">ReactJS</a></p>
   </footer>);
 }
 
